4a8a65ff2a18684850ba744577f42e640fd37c9f
apollomatheus <matheuslds1@gmail.com>
2019-09-22 19:00:00 -0300

    removed DOM

--- index.js
+++ index.js
@@ -10,27 +10,34 @@ const realtime_menu = new dropMenu({
 
 realtime_plugin.createData({});
 
+
 realtime_plugin.getData(function(data){
-  var toggle_viewer, hide_viewer, show_viewer, reload_viewer,
-      open_watcher, reload_viewer,
-      watcher;
+  var toggle_viewer, hide_viewer, show_viewer, load_viewer,
+      open_watcher, create_viewer, viewer;
+  
+  const defaultData = function (firstRun=false) {
+  	realtime_plugin.setData('path',null);
+  	realtime_plugin.setData('wasEnabled',(data.enabled && !firstRun));
+  	realtime_plugin.setData('enabled',false);
+  	realtime_plugin.setData('open',false);
+  }
+  defaultData(true);
   
   realtime_menu.setList({
     "button": "Realtime HTML",
     "list":{
        "Toggle viewer":{
-    	  id:"realtime-html-l",
-          click:function() {
+    	  id:"realtime-html-toggle",
+          click:function () {
             if (toggle_viewer) toggle_viewer();
           }
         },
        "Reload viewer":{
-    	  id:"realtime-html-l",
+    	  id:"realtime-html-reload",
           click:function() {
             //do not use reload_viewer
-            if (toggle_viewer) {
-              toggle_viewer();
-              toggle_viewer();
+            if (load_viewer && data.path && data.enabled) {
+           		load_viewer(data.path);
             }
           }
         },
@@ -50,89 +57,88 @@ realtime_plugin.getData(function(data){
       if (throwError) console.error(e);
     }
   };
-  reload_viewer = function () {
-    var viewer = document.getElementById('realtime_viewer');
-    var parent = viewer?viewer.parentNode:null;
-    if (viewer) {
-      viewer.parentNode.removeChild(viewer);
-    }
-    if (parent) {
-        parent.style.opacity = 0;
-        viewer = document.createElement('frame');
-        viewer.id = 'realtime_viewer';
-        viewer.className = 'html-viewer';
-        viewer.src = filepath;
-        parent.appendChild(viewer);
-        parent.style.opacity = 1;
+  
+  create_viewer = function () {
+    if (!data.open) {
+      viewer = new Tab({
+          id:'html_viewer',
+          type:'free',
+          name:'HTML VIEWER',
+          data:''
+      });
+    };
+    realtime_plugin.setData('open',true);
+  };
+  
+  load_viewer = function (f) {
+    if (f) {
+      fs.readFile(f,'utf8',function(err,data){
+        if (err) {
+          hide_viewer();
+          console.log('HTML_VIEWER::Failed to read file:\n\t=>',err);
+          return;
+        }
+        create_viewer();
+        viewer.setData(data);
+      	loadTab(document.getElementById('html_viewer_freeTab'));
+      });
     }
   };
-  show_viewer = function () {
-    var file = filepath;
-    if (!file) return false;
-    if (file.length > 5) {
-      if (file.slice(-5)=='.html') {
-        var viewer = document.createElement('frame');
-        var fset = document.createElement('frameset');
-        var content = document.getElementById('content_app');
-        if (!content) return false;
-        viewer.id = 'realtime_viewer';
-        viewer.className = 'html-viewer';
-        viewer.src = file;
-        fset.className = 'frameset-viewer';
-        fset.appendChild(viewer)
-        content.appendChild(fset);
-        return viewer;
+    
+  show_viewer = function (f) {
+    if (!f) return false;
+    if (f.length > 5) {
+      if (f.slice(-5)=='.html') {
+        load_viewer(f);
+        return true;
       }
     }
     return false;
   };
   
+  //manually hide viewer
   hide_viewer = function () {
-    var viewer = document.getElementById('realtime_viewer');
-    if (viewer) {
-      viewer.parentNode.parentNode.removeChild(viewer.parentNode);
-    }   
-    realtime_plugin.setData('enabled',false);
-    realtime_plugin.setData('path',null);
+    try {
+    	if (viewer && data.open) {
+          closeTab('html_viewer_freeTab');
+        }
+    } catch(e) {}
+    viewer=null;
+    defaultData();
     return false;
   };
   
-  open_watcher = function () {
-    clearInterval(watcher);
-    watcher = setInterval(function(){
-        if (data.path != filepath) {
-            var open=false;
-            try {
-              for(var t in tabs) { 
-                if (data.path == tabs[t].attributes.getNamedItem('longpath').value) {
-                  open = true;
-                  break;
-                }
-              }
-            }
-            catch (e) {}
-            if (!open) {
-              hide_viewer();
-              clearInterval(watcher);
-            }
+  document.addEventListener("file_saved",function(e){
+        if (filepath != data.path) return;
+        if (data.wasEnabled && !data.enabled) {
+         	 console.log('HTML_VIEWER::File closed?');
+             hide_viewer();
          } else {
-           reload_viewer();
+           load_viewer(data.path);
          }
-    },1500);
-  };
+  });
+  
+  document.addEventListener("tab_closed",function(e){
+      if (e.detail.tab.id=='html_viewer_freeTab') {
+        defaultData();
+      } else if (e.detail.tab.longpath==data.path && data.path) {
+        console.log('HTML_VIEWER::File closed?');
+        hide_viewer();
+        defaultData(true);
+      }
+  })
+  
                               
   toggle_viewer = function () {
     if (!data.enabled) {
-       if (show_viewer()) {
-         realtime_plugin.setData('path',filepath);
-      	 realtime_plugin.setData('enabled',true);
-                
-		 open_watcher();
-       }
-     } else {
-         hide_viewer();
-         clearInterval(watcher);
-     }
+       realtime_plugin.setData('path',filepath);
+       realtime_plugin.setData('enabled',true);
+       if (show_viewer(data.path)) {
+         console.log('HTML_VIEWER::Opening file => ',data.path);
+         return;
+       } 
+    }
+    hide_viewer();
   };
   
 });
